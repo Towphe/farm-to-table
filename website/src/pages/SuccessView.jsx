f329986b3963df2da046645112a6bd455f2a29ec
ALRnoche <arnoche1@up.edu.ph>
2024-05-19 21:57:24 +0800

    chore: added orderId & createdAt to SuccessView.jsx imported from orderController.js

--- website/src/pages/SuccessView.jsx
+++ website/src/pages/SuccessView.jsx
@@ -1,24 +1,35 @@
-import CustomerNavBar from "../components/common/CustomerNavbar";
-import Footer from "../components/common/Footer";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {Link} from 'react-router-dom';
 
-
-/*
-
-Mga need ilagay:
-* Hardcoded muna na order id number and date & time
-* Order Success! header
-* Go back button.... goes back to homepage
-
-*/
 
 function OrderSuccess()
 {
+    const [orderId, setOrderId] = useState('');
+    const [orderDate, setOrderDate] = useState('');
+
+    useEffect(() => 
+    {
+        // Fetch orderId and orderDate from the server when component mounts
+        axios.post('http://localhost:3000/api/order/confirm-order')
+            .then(response =>
+            {
+                const { orderId, createdAt } = response.data;
+                setOrderId(orderId);
+                setOrderDate(createdAt);
+            })
+            .catch(error => 
+            {
+                console.error('Error fetching order:', error);
+            });
+    }, []);
+
     return (
         <main className="w-full h-full flex flex-col justify-center items-center space-y-5">
             <text className="font-bold text-[28px]">Order success!</text>
             <p>Thank you for shopping with us!</p>
-            <p className="text-[14px]">Order #123456789 05/06/2024</p> {/* Will place dynamic constants here */}
-            <a href="/" className="bg-yellow-400 hover:bg-yellow-600 text-white font-bold py-2 px-20 rounded-full"> Go back </a>
+            <p className="text-[14px]">Order #{orderId} {orderDate}</p>
+            <Link to="/" className="bg-yellow-400 hover:bg-yellow-600 text-white font-bold py-2 px-20 rounded-full"> Go back </Link>
         </main>
     )
 }
