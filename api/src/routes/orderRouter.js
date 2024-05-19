470adb5f4d3a6b8b7d75d2a1418bcd5c3d8414fb
ALRnoche <arnoche1@up.edu.ph>
2024-05-19 21:54:42 +0800

    chore: modified confirmOrder link in orderRouter.js

--- api/src/routes/orderRouter.js
+++ api/src/routes/orderRouter.js
@@ -1,4 +1,4 @@
-import { findOrder, listOrders, createOrder } from "../controllers/orderController.js";
+import { findOrder, listOrders, createOrder, confirmOrder } from "../controllers/orderController.js";
 import { authenticateJWT } from "../middleware/authMiddleware.js";
 
 const baseUrl = "/api/order";
@@ -8,6 +8,7 @@ const orderRouter = (app) =>
     app.get(`${baseUrl}/:orderId`, findOrder);
     app.get(`${baseUrl}/ordered-items`, listOrders);
     app.post(`${baseUrl}/create-order`, authenticateJWT, createOrder);
+    app.post(`${baseUrl}/confirm-order`, confirmOrder);
 }
 
 export default orderRouter;
