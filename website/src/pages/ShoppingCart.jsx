2de3197f192abbe3cf2ce179d668049dd0fd1f1d
Jandayl <jbyasis@up.edu.ph>
2024-05-19 15:50:24 +0800

    chore: changed appearance of total

--- website/src/pages/ShoppingCart.jsx
+++ website/src/pages/ShoppingCart.jsx
@@ -50,9 +50,9 @@ function ShoppingCart() {
     return (
         <main className="relative w-full h-full overflow-x-hidden gap-6 mt-10">
             <h1 className="w-screen h-auto text-center absolute top-4 block m-4 font-bold text-3xl">Shopping Cart</h1>
-            <div className="w-screen h-auto flex flex-shrink-0 flex-col items-center gap-6 md:flex-col justify-center mt-24">
+            <div className="w-screen h-auto flex flex-shrink-0 flex-col items-center gap-3 md:flex-col justify-center mt-24">
                 {products.map((product) => (
-                    <div key={product._id} className="flex flex-col text-m flex-shrink-0 items-center justify-center shadow-md rounded-t-md border-black-50 bg-opacity-40 bg-green-300 p-3">
+                    <div key={product._id} className="flex flex-col text-m flex-shrink-0 items-center justify-center shadow-md rounded-t-md border-black-50 bg-opacity-40 bg-green-300 p-2">
                         <div className="flex justify-between w-full p-2 space-x-3">
                             <span className="space-x-3">
                                 <Link to={`/products/${product._id}`}>{product.productName}</Link>
@@ -67,7 +67,7 @@ function ShoppingCart() {
                         </div>
                     </div>
                 ))}
-                <div className="flex justify-between space-x-40 text-2xl font-bold">
+                <div className="flex justify-between space-x-40 text-1xl">
                     <span>Total:</span>
                     <span>P{total}</span>
                 </div>
