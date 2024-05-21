797e0ef6fb1d797a605bd07d9222cc8b6b103f2e
ALRnoche <arnoche1@up.edu.ph>
2024-05-21 22:28:27 +0800

    chore: added base file for OrderDetail.jsx

---
+++ website/src/pages/OrderDetail.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
+
+function OrderDetail() 
+{}
+
+export default OrderDetail;
