8f21893349a6ab88b3913422ad9275014ed280be
ALRnoche <arnoche1@up.edu.ph>
2024-05-21 22:26:38 +0800

    chore: added templated format of checkout view

--- website/src/pages/CheckoutView.jsx
+++ website/src/pages/CheckoutView.jsx
@@ -1,29 +1,48 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import {Link} from 'react-router-dom';
-
-/*
-
-Mga need ilagay:
-* Import mga constants from products
-* Input fields for payment, delivery info., etc.
-* Order button na magredirect sa SuccessView.jsx
-
-*/
+import { Link } from 'react-router-dom';
 
 function CheckoutView() 
 {
-    return (
-        <main className="w-full h-full flex flex-col justify-center items-center">       
-            <text className="font-bold text-[28px]">Checkout</text>
-            <p>Items</p> {/* Will place constants here */}
-            <p>Payment</p> {/* Will place constants here */}
-            <p>Delivery info</p> {/* Will place constants here */}
-            <a href="/success" className="bg-green-700 hover:bg-green-400 text-white font-bold py-2 px-30 rounded-full inline-block" 
-            style={{ minWidth: '300px', textAlign: 'center' }}> Order </a>
-        </main>
-    )
+    const items = 
+    [
+        {id: 12000, name: 'Jaco', price: 100},
+        {id: 11330, name: 'Jaco', price: 100},
+        {id: 21330, name: 'Jaco', price: 100},
+        {id: 12330, name: 'Jaco', price: 100}
+    ]
+  return (
+    <main className="flex flex-col min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold mb-4">Checkout</h1>
+
+      <section className="flex flex-col border border-gray-300 rounded-md p-4 mb-4">
+        <h2 className="text-lg font-bold mb-2">Items</h2>
+        {items.map((item) => 
+        (
+          <div key={item.id} className="flex mb-2">
+            <p className="w-1/2 text-gray-700 truncate">{item.name}</p>
+            <p className="w-1/2 text-right font-medium text-gray-700">₱{item.price}</p>
+          </div>
+        ))}
+      </section>
+
+      <section className="flex flex-col border border-gray-300 rounded-md p-4 mb-4">
+        <h2 className="text-lg mb-2">Payment</h2>
+      </section>
+
+      <section className="flex flex-col border border-gray-300 rounded-md p-4 mb-4">
+        <h2 className="text-lg mb-2">Delivery Info</h2>
+        <label className="mb-4">House number / Unit no.</label>
+        <h3 className="mb-4">Street</h3>
+        <h3 className="mb-4">Baranggay</h3>
+        <h3 className="mb-4">City</h3>
+        <h3 className="mb-4">Province</h3>
+      </section>
+      <Link to="/success" className="bg-green-700 hover:bg-green-400 text-white font-bold py-2 px-4 rounded-full inline-flex justify-center items-center w-full">
+        Order 
+        </Link>
+    </main>
+  );
 }
 
 export default CheckoutView;
-
